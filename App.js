26b43959c4551b426d9e9c391b4ef8264b577e63
dilipkumar5 <dilipkumargms27@gmail.com>
2025-08-07 19:31:46 -0400

    ignited the app manually without create-react-app

--- App.js
+++ App.js
@@ -4,11 +4,17 @@
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
 
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", {id: "parent"}, [
     React.createElement("div", {id: "child"}, [
         React.createElement("h1", {}, "I'm an h1 tag"),
         React.createElement("h2", {}, "I'm an h2 tag")
+    ]),
+    React.createElement("div", {id: "child2"}, [
+        React.createElement("h1", {}, "I'm a h1 tag"),
+        React.createElement("h2", {}, "I'm a h2 tag")
     ])
 ]);
 
@@ -16,4 +22,4 @@ console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(parent)
